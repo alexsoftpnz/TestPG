022eaed06442fee3073b442986343e234b8d8bd0
a.loshchinin <a.loshchinin@andersenlab.com>
2020-02-03 09:47:13 +0300

    Code Review

--- f_goo.sql
+++ f_goo.sql
@@ -1,11 +1,11 @@
 create or replace function f_goo (p_param1 in int)--, p_line out text, p_tx out text )
-returns record
+	returns record
 as
-    $$
-    begin
-		select	nm::text	as nm,
-		    	'Test'::text	as tm
-        from generate_series(1,p_param1) t(nm);
-	end;
-    $$
-language plpgsql;
+$$
+begin
+	select nm::text  as nm,
+		'Test'::text as tm
+	from generate_series(1,p_param1) t(nm);
+end;
+$$
+	language plpgsql;
